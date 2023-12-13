5438ce1ae539d2ee4e55eee952c41469423b9625
Ryan <ryan.v050358@gmail.com>
2023-12-13 18:50:19 +1100

    chore: change to default credentials

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
-    $host = "feenix-mariadb.swin.edu.au";
-    $user = "s103511424";
-    $pswd = "050503";
-    $dbnm = "s103511424_db";
+    $host = "localhost";
+    $user = "root";
+    $pswd = "";
+    $dbnm = "friends_system";
 ?>
